a6686d6804a03d141e8192a77fa893a8fc6b789d
BLAKES_ALIEN\WindowsDropbear <blake.kammermanns@connect.qut.edu.au>
2018-01-27 02:47:27 +1000

    Finished the tablet, placed it in the prefab folder

---
+++ Assets/BlakeStuffStayOut/BlakeScripts/TranslationTablet.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslationTablet : MonoBehaviour {
+
+	public GameObject[] screens;
+	public float transitionTime;
+	public float amountToMove;
+	public float speed;
+
+	int currentScreen;
+	bool moving;
+	Vector3 startPos;
+	Vector3 newPos;
+	float accumulationTime = 0;
+
+
+	void Start(){
+		startPos = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(moving){
+			transform.localPosition = Vector3.Lerp(transform.localPosition, newPos, Time.deltaTime * speed);
+			if(Vector3.Distance(transform.localPosition,newPos) < .1){
+				transform.localPosition = newPos;
+				moving = false;
+			}
+//			transform.localPosition = Vector3.Lerp(transform.localPosition, newPos, Mathf.SmoothStep(0, 1, accumulationTime/transitionTime));
+//			accumulationTime += Time.deltaTime;
+
+		}
+
+	}
+
+	public void MoveLeft(){
+		if(currentScreen < screens.Length - 1){
+			currentScreen += 1;
+			moving = true;
+			newPos = new Vector3(amountToMove * currentScreen, 0, 0);
+			accumulationTime = 0;
+		}
+	}
+
+	public void MoveRight(){
+		
+		if(currentScreen > 0){
+			currentScreen -= 1;
+			moving = true;
+			newPos = new Vector3(amountToMove * currentScreen, 0, 0);
+			accumulationTime = 0;
+		}
+	}
+
+}
