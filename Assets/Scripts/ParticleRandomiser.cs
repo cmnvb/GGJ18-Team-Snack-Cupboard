e0cc90c50614fd69105c73d2dcf8335345a5c449
cmnvb <superstar.kim@hotmail.com>
2018-01-28 11:41:51 +1000

    Add particle randomiser

---
+++ Assets/Scripts/ParticleRandomiser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleRandomiser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("RandomParticleEvent");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private IEnumerator RandomParticleEvent() {
+		while (true) {
+			yield return new WaitForSeconds(Random.Range(1f, 3.5f));
+			GetComponent<ParticleSystem>().Play();
+			GetComponent<AudioSource>().Play();
+		}
+	}
+}
